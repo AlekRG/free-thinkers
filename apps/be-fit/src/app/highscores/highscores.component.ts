1e7b45049d6db6807ac4fb86ff7a8b0655bfca32
Aleksandar.Grujicic <Aleksandar.Grujicic@fisglobal.com>
2021-05-25 18:21:47 +0200

    Add more users

--- apps/be-fit/src/app/highscores/highscores.component.ts
+++ apps/be-fit/src/app/highscores/highscores.component.ts
@@ -21,7 +21,7 @@ export class HighscoresComponent implements OnInit {
     {
       name: 'Hana',
       updated: new Date('1/1/16'),
-      count: 11230,
+      count: 13230,
       rank: 22,
       image: '/assets/user 3 Hana.jpg',
       trend: 0,
@@ -29,7 +29,7 @@ export class HighscoresComponent implements OnInit {
     {
       name: 'Ljubica',
       updated: new Date('1/1/16'),
-      count: 1564,
+      count: 11004,
       rank: 23,
       image: '/assets/user 1 Ljubica.jpg',
       trend: 0,
@@ -37,7 +37,7 @@ export class HighscoresComponent implements OnInit {
     {
       name: 'Aleksandar',
       updated: new Date('1/1/16'),
-      count: 78,
+      count: 12030,
       rank: 24,
       image: '/assets/user 2 Aleksandar.jpg',
       trend: 1,
@@ -45,12 +45,112 @@ export class HighscoresComponent implements OnInit {
     {
       name: 'Veljko',
       updated: new Date('1/1/16'),
-      count: 11,
+      count: 10236,
       rank: 25,
       image: '/assets/user 4 Veljko.jpg',
       trend: 1,
     },
-  ];
+    {
+      name: 'Marko',
+      updated: new Date('1/1/16'),
+      count: 1546,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Wassim',
+      updated: new Date('1/1/16'),
+      count: 1112,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Strahinja',
+      updated: new Date('1/1/16'),
+      count: 12853,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Uros',
+      updated: new Date('1/1/16'),
+      count: 7894,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Milos',
+      updated: new Date('1/1/16'),
+      count: 3369,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Ivan',
+      updated: new Date('1/1/16'),
+      count: 357,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Marwen',
+      updated: new Date('1/1/16'),
+      count: 1596,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Steven',
+      updated: new Date('1/1/16'),
+      count: 1758,
+      rank: 25,
+
+      trend: 1,
+    },
+    {
+      name: 'Zvezdana',
+      updated: new Date('1/1/16'),
+      count: 6654,
+      rank: 25,
+
+      trend: 1,
+    },
+    {
+      name: 'Suncica',
+      updated: new Date('1/1/16'),
+      count: 13540,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Ruzica',
+      updated: new Date('1/1/16'),
+      count: 9099,
+      rank: 25,
+      trend: 1,
+    },
+    {
+      name: 'Miroslav',
+      updated: new Date('1/1/16'),
+      count: 11,
+      rank: 25,
+      trend: 0,
+    },
+    {
+      name: 'Nikola',
+      updated: new Date('1/1/16'),
+      count: 4567,
+      rank: 25,
+      trend: 0,
+    },
+    {
+      name: 'Ana',
+      updated: new Date('1/1/16'),
+      count: 987,
+      rank: 25,
+      trend: 1,
+    },
+  ].sort((a, b) => (a.count < b.count) ? 1 : -1);
 
   constructor() {}
 
